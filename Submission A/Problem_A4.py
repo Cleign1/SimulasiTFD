9bbed01530ce6d985fa4bfa872ef2a5f54f454fa
Ibnu Khaidar <ibnukhaidar@gmail.com>
2024-05-25 21:05:21 +0700

    add problem a1 and implement problem a4

--- Submission A/Problem_A4.py
+++ Submission A/Problem_A4.py
@@ -20,17 +20,21 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 def solution_A4():
     imdb, info = tfds.load("imdb_reviews", with_info=True, as_supervised=True)
     # YOUR CODE HERE
+    train_data, test_data = imdb['train'], imdb['test']
 
-    # DO NOT CHANGE THIS CODE
+    # YOUR CODE HERE
+    train_sentences = []
+    testing_sentences = []
+    train_label = []
+    testing_label = []
+
     for s, l in train_data:
-        training_sentences.append(s.numpy().decode('utf8'))
-        training_labels.append(l.numpy())
+        train_sentences.append(s.numpy().decode('utf8'))
+        train_label.append(l.numpy())
 
     for s, l in test_data:
         testing_sentences.append(s.numpy().decode('utf8'))
-        testing_labels.append(l.numpy())
-
-    # YOUR CODE HERE
+        testing_label.append(l.numpy())
 
     # DO NOT CHANGE THIS CODE
     # Make sure you used all of these parameters or test may fail
@@ -41,12 +45,35 @@ def solution_A4():
     oov_tok = "<OOV>"
 
     # Fit your tokenizer with training data
-    tokenizer =  # YOUR CODE HERE
+    # YOUR CODE HERE
+    tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+    tokenizer.fit_on_texts(train_sentences)
+
+    train_sequences = tokenizer.texts_to_sequences(train_sentences)
+    train_padded = pad_sequences(train_sequences, maxlen=max_length, truncating=trunc_type)
+
+    test_sequences = tokenizer.texts_to_sequences(testing_sentences)
+    test_padded = pad_sequences(test_sequences, maxlen=max_length)
+
+    train_labels_final = np.array(train_label)
+    test_labels_final = np.array(testing_label)
 
     model = tf.keras.Sequential([
         # YOUR CODE HERE. Do not change the last layer.
+        tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(32, activation='relu'),
+        tf.keras.layers.Dropout(0.1),
         tf.keras.layers.Dense(1, activation='sigmoid')
     ])
+
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+    
+    model.fit(train_padded, train_labels_final,
+              epochs=10,
+              validation_data=(test_padded, test_labels_final))
 
     return model
 
