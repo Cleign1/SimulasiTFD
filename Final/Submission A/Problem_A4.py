87d65d4089c117e2202cdef987976b4bbfe9ce36
Ibnu Khaidar <ibnukhaidar@gmail.com>
2024-05-26 00:45:30 +0700

    finalize for submission A

---
+++ Final/Submission A/Problem_A4.py
@@ -0,0 +1,85 @@
+# ==========================================================================================================
+# PROBLEM A4
+
+# Build and train a binary classifier for the IMDB review dataset.
+# The classifier should have a final layer with 1 neuron activated by sigmoid.
+# Do not use lambda layers in your model.
+
+# The dataset used in this problem is originally published in http://ai.stanford.edu/~amaas/data/sentiment/
+
+# Desired accuracy and validation_accuracy > 83%
+# ===========================================================================================================
+
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import numpy as np
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+
+def solution_A4():
+    imdb, info = tfds.load("imdb_reviews", with_info=True, as_supervised=True)
+    # YOUR CODE HERE
+    train_data, test_data = imdb['train'], imdb['test']
+
+    # YOUR CODE HERE
+    train_sentences = []
+    testing_sentences = []
+    train_label = []
+    testing_label = []
+
+    for s, l in train_data:
+        train_sentences.append(s.numpy().decode('utf8'))
+        train_label.append(l.numpy())
+
+    for s, l in test_data:
+        testing_sentences.append(s.numpy().decode('utf8'))
+        testing_label.append(l.numpy())
+
+    # DO NOT CHANGE THIS CODE
+    # Make sure you used all of these parameters or test may fail
+    vocab_size = 10000
+    embedding_dim = 16
+    max_length = 120
+    trunc_type = 'post'
+    oov_tok = "<OOV>"
+
+    # Fit your tokenizer with training data
+    # YOUR CODE HERE
+    tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+    tokenizer.fit_on_texts(train_sentences)
+
+    train_sequences = tokenizer.texts_to_sequences(train_sentences)
+    train_padded = pad_sequences(train_sequences, maxlen=max_length, truncating=trunc_type)
+
+    test_sequences = tokenizer.texts_to_sequences(testing_sentences)
+    test_padded = pad_sequences(test_sequences, maxlen=max_length)
+
+    train_labels_final = np.array(train_label)
+    test_labels_final = np.array(testing_label)
+
+    model = tf.keras.Sequential([
+        # YOUR CODE HERE. Do not change the last layer.
+        tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(6, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+    
+    model.fit(train_padded, train_labels_final,
+              epochs=10,
+              validation_data=(test_padded, test_labels_final))
+
+    return model
+
+
+# The code below is to save your model as a .h5 file.
+# It will be saved automatically in your Submission folder.
+if __name__ == '__main__':
+    # DO NOT CHANGE THIS CODE
+    model = solution_A4()
+    model.save("model_A4.h5")
