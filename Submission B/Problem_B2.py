135533d81663e7b2761646f3208e2b35f21cf9d4
Ibnu Khaidar <ibnukhaidar@gmail.com>
2024-05-26 13:56:43 +0700

    Done with Problem B2

--- Submission B/Problem_B2.py
+++ Submission B/Problem_B2.py
@@ -16,15 +16,35 @@ import tensorflow as tf
 
 def solution_B2():
     fashion_mnist = tf.keras.datasets.fashion_mnist
+    (training_images, training_labels), (test_image, test_label) = fashion_mnist.load_data()
 
     # NORMALIZE YOUR IMAGE HERE
+    training_images = training_images.reshape(60000, 28, 28, 1)
+    training_images = training_images / 255.0
+
+    test_image = test_image.reshape(10000, 28, 28, 1)
+    test_image = test_image / 255.0
 
     # DEFINE YOUR MODEL HERE
     # End with 10 Neuron Dense, activated by softmax
+    model = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(filters= 32, kernel_size=(3,3), activation='relu',input_shape=(28, 28, 1)),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv2D(filters= 32, kernel_size=(3,3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(units= 128, activation='relu'),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(units=10, activation='softmax')
+    ])
 
     # COMPILE MODEL HERE
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
     # TRAIN YOUR MODEL HERE
+    model.fit(training_images, training_labels, validation_data=(test_image, test_label),epochs=10)
 
     return model
 
