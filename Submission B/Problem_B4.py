a6e378590f3e229a6d6a5327d21efaa8fb04b15a
Ibnu Khaidar <ibnukhaidar@gmail.com>
2024-05-26 21:07:45 +0700

    done with problem B5

--- Submission B/Problem_B4.py
+++ Submission B/Problem_B4.py
@@ -15,10 +15,14 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import tensorflow as tf
 import pandas as pd
+import csv
+import urllib
+import numpy as np
 
 
 def solution_B4():
-    bbc = pd.read_csv('https://github.com/dicodingacademy/assets/raw/main/Simulation/machine_learning/bbc-text.csv')
+    bbc = ('https://github.com/dicodingacademy/assets/raw/main/Simulation/machine_learning/bbc-text.csv')
+    urllib.request.urlretrieve(bbc, 'bbc-text.csv')
 
     # DO NOT CHANGE THIS CODE
     # Make sure you used all of these parameters or you can not pass this test
@@ -32,22 +36,74 @@ def solution_B4():
 
     # YOUR CODE HERE
     # Using "shuffle=False"
+    stopwords = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "as", "at", "be", "because", "been", "before", "being", "below", "between",
+                "both", "but", "by", "could", "did", "do", "does", "doing", "down", "during", "each", "few", "for", "from", "further", "had", "has", "have", "having", "he", "he'd",
+                "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "it",
+                "it's", "its", "itself", "let's", "me", "more", "most", "my", "myself", "nor", "of", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out",
+                "over", "own", "same", "she", "she'd", "she'll", "she's", "should", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves",
+                "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", 
+                "we", "we'd", "we'll", "we're", "we've", "were", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", 
+                "with", "would", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves" ]
+
+    sentences = []
+    labels =[]
+
+    with open('bbc-text.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        next(reader)
+        for row in reader:
+            labels.append(row[0])
+            sentence = row[1]
+            for word in stopwords:
+                token = " " + word + " "
+                sentence = sentence.replace(token, " ")
+            sentences.append(sentence)
     
-    training_sentences, validation_sentences = #YOUR CODE HERE
-    training_labels, validation_labels = #YOUR CODE HERE
+    train_size = int(len(sentences) * training_portion)
+
+    train_sentences = sentences[:train_size]
+    train_labels = labels[:train_size]
+
+    validation_sentences = sentences[train_size:]
+    validation_labels = labels[train_size:]
 
     # Fit your tokenizer with training data
-    tokenizer =  # YOUR CODE HERE
+    # You can also use Tokenizer to encode your label.
+    # YOUR CODE HERE
+    tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+    tokenizer.fit_on_texts(train_sentences)
+    word_index = tokenizer.word_index
     
     # You can also use Tokenizer to encode your label.
+    train_sequences = tokenizer.texts_to_sequences(train_sentences)
+    train_padded = pad_sequences(train_sequences, padding=padding_type, maxlen=max_length)
+
+    validation_sequences = tokenizer.texts_to_sequences(validation_sentences)
+    validation_padded = pad_sequences(validation_sequences, padding=padding_type, maxlen=max_length)
+    
+    label_tokenizer = Tokenizer()
+    label_tokenizer.fit_on_texts(labels)
+
+    train_label_seq = np.array(label_tokenizer.texts_to_sequences(train_labels))
+    validation_label_seq = np.array(label_tokenizer.texts_to_sequences(validation_labels))
 
     model = tf.keras.Sequential([
         # YOUR CODE HERE.
         # YOUR CODE HERE. DO not change the last layer or test may fail
+        tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+        tf.keras.layers.GlobalAveragePooling1D(),
+        tf.keras.layers.Dense(24, activation='relu'),
         tf.keras.layers.Dense(6, activation='softmax')
     ])
 
     # Make sure you are using "sparse_categorical_crossentropy" as a loss fuction
+    model.compile(loss='sparse_categorical_crossentropy',
+                optimizer='adam',
+                metrics=['accuracy'])
+    
+    model.fit(train_padded, train_label_seq,
+              epochs=50,
+              validation_data = (validation_padded, validation_label_seq))
 
     return model
 
