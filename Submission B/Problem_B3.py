e3b7d633876ab9e4262bdbe4184878d4ce9378b9
Ibnu Khaidar <ibnukhaidar@gmail.com>
2024-05-26 21:28:11 +0700

    uncomment downloaded file

--- Submission B/Problem_B3.py
+++ Submission B/Problem_B3.py
@@ -19,12 +19,12 @@ from keras.preprocessing.image import ImageDataGenerator
 
 
 def solution_B3():
-    # data_url = 'https://github.com/dicodingacademy/assets/releases/download/release-rps/rps.zip'
-    # urllib.request.urlretrieve(data_url, 'rps.zip')
-    # local_file = 'rps.zip'
-    # zip_ref = zipfile.ZipFile(local_file, 'r')
-    # zip_ref.extractall('data/')
-    # zip_ref.close()
+    data_url = 'https://github.com/dicodingacademy/assets/releases/download/release-rps/rps.zip'
+    urllib.request.urlretrieve(data_url, 'rps.zip')
+    local_file = 'rps.zip'
+    zip_ref = zipfile.ZipFile(local_file, 'r')
+    zip_ref.extractall('data/')
+    zip_ref.close()
 
     # YOUR CODE HERE
     TRAINING_DIR = "data/rps"
