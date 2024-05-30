ff172194c1b1e2e44552fe8a8a4b4ba4bcca3560
Ibnu Khaidar <ibnukhaidar@gmail.com>
2024-05-30 12:26:47 +0700

    updated gitignore and done with C2

--- Submission C/Problem_C2.py
+++ Submission C/Problem_C2.py
@@ -15,14 +15,39 @@ import tensorflow as tf
 def solution_C2():
     mnist = tf.keras.datasets.mnist
 
+
     # NORMALIZE YOUR IMAGE HERE
+    (training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+    training_images = training_images / 255.0
+    test_images = test_images / 255.0
+
+    class custom_callback(tf.keras.callbacks.Callback):
+        def on_epoch_end(self, epoch, logs={}):
+            if (logs.get('accuracy') > 0.91 and logs.get('val_accuracy') > 0.91 and logs.get('loss') < 0.1):
+                print("\nTraining Selesai")
+                self.model.stop_training = True
+
 
     # DEFINE YOUR MODEL HERE
     # End with 10 Neuron Dense, activated by softmax
+    model = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(28, 28, 1)),
+        tf.keras.layers.MaxPooling2D(2,2),
+        tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2,2),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(10, activation='softmax')
+    ])
+
+    callback = custom_callback()
 
     # COMPILE MODEL HERE
+    model.compile(optimizer='adam', loss="sparse_categorical_crossentropy", metrics=['accuracy'])
 
     # TRAIN YOUR MODEL HERE
+    model.fit(training_images, training_labels, validation_data=(test_images, test_labels), epochs=300,callbacks=callback
+              )
 
     return model
 
