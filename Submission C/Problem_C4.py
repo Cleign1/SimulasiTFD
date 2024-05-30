6b8ef11e93cb38f60250d409989f4d3f950df11a
Ibnu Khaidar <ibnukhaidar@gmail.com>
2024-05-30 13:43:34 +0700

    updated gitignore and finished C4

--- Submission C/Problem_C4.py
+++ Submission C/Problem_C4.py
@@ -19,6 +19,13 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 
+class custom_callback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if (logs.get('accuracy') > 0.81 and logs.get('val_accuracy') > 0.81):
+            print("\nTraining Selesai")
+            self.model.stop_training = True
+
+
 def solution_C4():
     data_url = 'https://github.com/dicodingacademy/assets/raw/main/Simulation/machine_learning/sarcasm.json'
     urllib.request.urlretrieve(data_url, 'sarcasm.json')
@@ -33,17 +40,60 @@ def solution_C4():
     oov_tok = "<OOV>"
     training_size = 20000
 
+    with open('sarcasm.json', 'r') as f:
+        words = json.load(f)
+
     sentences = []
     labels = []
     # YOUR CODE HERE
 
+    for item in words:
+        sentences.append(item['headline'])
+        labels.append(item['is_sarcastic'])
+
+    training_sentences = sentences[:training_size]
+    training_labels = labels[:training_size]
+    test_sentences = sentences[training_size:]
+    test_labels = labels[training_size:]
+
     # Fit your tokenizer with training data
-    tokenizer =  # YOUR CODE HERE
+    # YOUR CODE HERE
+    tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+    tokenizer.fit_on_texts(training_sentences)
+
+    word_index = tokenizer.word_index
+
+    training_sequences = tokenizer.texts_to_sequences(training_sentences)
+    training_padded = pad_sequences(training_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+    test_sequences = tokenizer.texts_to_sequences(test_sentences)
+    test_padded = pad_sequences(test_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+    training_padded = np.array(training_padded)
+    training_labels = np.array(training_labels)
+    test_padded = np.array(test_padded)
+    test_labels = np.array(test_labels)
+
 
     model = tf.keras.Sequential([
         # YOUR CODE HERE. DO not change the last layer or test may fail
+        tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+        tf.keras.layers.GlobalAveragePooling1D(),
+        tf.keras.layers.Dense(24, activation='relu'),
         tf.keras.layers.Dense(1, activation='sigmoid')
     ])
+
+    callbacks = custom_callback()
+
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam', metrics=['accuracy'])
+
+    model.fit(training_padded, training_labels,
+              epochs=50,
+              validation_data=(test_padded, test_labels),
+              callbacks=callbacks
+              )
+
     return model
 
 
